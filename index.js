699b40a0b4e708737ec7388bdb20c5dc4a37b194
hamana030 <scv943@naver.com>
2020-08-17 13:56:40 +0900

    set js practice

---
+++ index.js
@@ -0,0 +1,13 @@
+const nicoInfo = {
+  name: "Nicolas",
+  age: "55",
+  gender: "male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "LOTR", "Oldboy"],
+  favFood: [
+    { name: "Kimchi", fatty: false },
+    { name: "Cheese burger", fatty: true },
+  ],
+};
+
+console.log(nicoInfo);
